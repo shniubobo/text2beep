d8ddda6e2c962acee1a30eb1cf239115f31a2f00
shniubobo <shniubobo@outlook.com>
2020-08-31 22:33:03 +0800

    feat(__main__): Add option --quiet

--- text2beep/__main__.py
+++ text2beep/__main__.py
@@ -33,6 +33,8 @@ def _get_args():
     parser.add_argument('-h', '--help', action='help',
                         default=argparse.SUPPRESS,
                         help='Show this help message and exit.')
+    parser.add_argument('-q', '--quiet', action='store_true', dest='quiet',
+                        help='Suppress all logs. Overwrite --verbose.')
     parser.add_argument('-v', '--verbose', action='store_true', dest='verbose',
                         help='Enable verbose logging (default to off).')
     parser.add_argument('-V', '--version', action='store_true', dest='version',
@@ -43,7 +45,9 @@ def _get_args():
 
 
 def _parse_args(args):
-    if args['verbose']:
+    if args['quiet']:
+        logging.basicConfig(level=60)
+    elif args['verbose']:
         logging.basicConfig(level=logging.DEBUG,
                             format='%(levelname)s:%(name)s:%(threadName)s:'
                                    '%(lineno)d:%(message)s')
